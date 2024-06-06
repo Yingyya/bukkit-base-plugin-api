682a0b31ca9dd38bc940319ef7eb4d05a6f8b5fe
Yingyya <yyaiwy666@163.com>
2024-06-06 08:20:35 +0800

    modify ILevelDB.java

--- src/main/java/cn/yingyya/bukkit/base/api/ILevelDB.java
+++ src/main/java/cn/yingyya/bukkit/base/api/ILevelDB.java
@@ -1,8 +1,12 @@
 package cn.yingyya.bukkit.base.api;
 
+import java.util.List;
+
 public interface ILevelDB {
 
 	void close();
 	byte[] get(byte[] key);
 	void put(byte[] key, byte[] value);
+
+	List<String> getDBInfo();
 }
